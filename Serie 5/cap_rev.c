5a5b50c8846982cc8a8fef2d5a5d2635aac1c102
flxw <ich@flxw.de>
2013-01-10 11:59:59 +0000

    Aufgabe 1 fertig!
    Um Pointerkopfschmerzen zu vermeiden, wurden die Funktionen von
    invert.{h,c} durch einen kleinen Fünfzeiler ersetzt.

--- Serie 5/cap_rev.c
+++ Serie 5/cap_rev.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <invert.h>
+#include <ctype.h>
+#include <string.h>
 
-int main( int argc, char * argv[v] ) {
+int main( int argc, char** argv ) {
 	if( argc == 1) {
 		printf("Bitte Funktion mit Parametern aufrufen!");
 		return 1;
 	} 
-	for( int i=1; i<argc; i++) {
-		argv[i]=invert(argv[i]);
-	}
-	for( int i=argc; i>0; i--) {
-		printf(%c; \n; argv[i]);
-	}
+
+    int i, j;
+    for (i=argc-1; i>=1; --i) {
+        for (j=strlen(argv[i]); j >= 0; --j) {
+            printf("%c", toupper(argv[i][j]));
+        } 
+        printf(" ");
+    }
+    printf("\n");
+
 	return 0;
 }		
--- Serie 5/invert.h
+++
@@ -1,8 +0,0 @@
-# ifndef __invert_h
-# define __invert_h
-
-# define INVERT "invert"
-
-char[] invert (char[] a);
-
-#endif
