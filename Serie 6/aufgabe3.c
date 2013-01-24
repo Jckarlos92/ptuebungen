3b5a823b0e4e75ef655339d02bc13457772279e9
mr-flannery <dhempfing@ubuntu.(none)>
2013-01-24 13:08:28 +0100

    aufgabe3.c geändert
    aufgabe3.c funktioniert mit Integer, ist allerdings noch nicht auf die Datentypen von aufgabe2.c angepasst, dies muss noch gemacht werden.

--- Serie 6/aufgabe3.c
+++ Serie 6/aufgabe3.c
@@ -7,11 +7,12 @@ int main (int argc, char * argv[]) {
 	int *zahlen = (int *) malloc (sizeof(int) * argc);
 	int *qzahlen = (int *) malloc (sizeof(int) *argc);
 	int i;
-	for (i=0;i<argc;i++) {
-		*(zahlen+i) = atoi(argv[i]);
+	for (i=0;i<argc-1;i++) {
+		*(zahlen+i) = atoi(argv[i+1]);
+		*(qzahlen+i) = zahlen[i]*zahlen[i];
 	}
-	for (i=0;i<argc;i++) {
-		printf("%d \n", zahlen[i]);
+	for (i=0;i<argc-1;i++) {
+		printf("%d, %d \n", zahlen[i], qzahlen[i]);
 	}
 	return 0;
 }
