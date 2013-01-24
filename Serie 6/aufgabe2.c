269ed0bf1fb775c76e14aa270d8e9648204f3708
mr-flannery <dhempfing@ubuntu.(none)>
2013-01-24 11:41:59 +0100

    aufgabe2.c optimiert in Hinsicht auf Speichernutzung

--- Serie 6/aufgabe2.c
+++ Serie 6/aufgabe2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main (int argc, char * argv[]) {
-	int zahlen[100];
-	int qzahlen[100];
+	unsigned char zahlen[100];
+	short qzahlen[100];
 	int i;
 	for (i=0; i<100; i++) {
-		zahlen[i]=i+1;
+		zahlen[i]=(char)(i+1);
 		qzahlen[i]=(i+1)*(i+1);
 	} 
 	for (i=0; i<100; i++) {
