d038b10aeae0adacce987f3e0087d1ae0039783f
mr-flannery <dhempfing@ubuntu.(none)>
2013-01-24 13:18:34 +0100

    aufgabe3.c geändert
    aufgabe3.c arbeitet jetzt mit den gleichen Datentypen wie aufgabe2.c. Short wurde zu unsigned short "erweitert", um Parameter bis 255 als positive Quadratzahlen darstellen zu können. Für Parameter >= 256 gibt es 0 aus (logischerweise). Kommentar dazu muss noch im Quellcode hinzugefügt werden.

--- Serie 6/aufgabe3.c
+++ Serie 6/aufgabe3.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//argv[0] rausnehmen!!!!
-
 int main (int argc, char * argv[]) {
-	int *zahlen = (int *) malloc (sizeof(int) * argc);
-	int *qzahlen = (int *) malloc (sizeof(int) *argc);
+	unsigned char *zahlen = (unsigned char *) malloc (sizeof(unsigned char) * argc);
+	unsigned short *qzahlen = (unsigned short *) malloc (sizeof(unsigned short) *argc);
 	int i;
 	for (i=0;i<argc-1;i++) {
 		*(zahlen+i) = atoi(argv[i+1]);
-		*(qzahlen+i) = zahlen[i]*zahlen[i];
+		*(qzahlen+i) =(short) zahlen[i]*zahlen[i];
 	}
 	for (i=0;i<argc-1;i++) {
 		printf("%d, %d \n", zahlen[i], qzahlen[i]);
