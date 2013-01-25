5aa9734f07c7bf0e059000492f5288b6a14a7378
mr-flannery <dhempfing@ubuntu.(none)>
2013-01-25 11:35:33 +0100

    aufgabe3.c fertig gestellt!
    aufgabe3.c wurde um die free()-Befehle erweitert, um allozierten Speicher wieder
    freizugeben. Ich habe noch einen einleitenden Kommentar bzgl. der Datentypen geschrieben.

--- Serie 6/aufgabe3.c
+++ Serie 6/aufgabe3.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Da wir in Aufgabe 2 mit unsigned char und short gearbeitet haben, haben wir dies auch
+in diesem Programm übernommen, um eine gewisse Speichereffizienz beizubehalten. Allerdings
+haben wir unsigned short statt short verwendet, um alle Zahlen (0-255), die unsigned char 
+fasst, auch als positive Quadratzahlen darstellen zu können. */
+
 int main (int argc, char * argv[]) {
 	unsigned char *zahlen = (unsigned char *) malloc (sizeof(unsigned char) * argc);
 	unsigned short *qzahlen = (unsigned short *) malloc (sizeof(unsigned short) *argc);
@@ -12,5 +17,7 @@ int main (int argc, char * argv[]) {
 	for (i=0;i<argc-1;i++) {
 		printf("%d, %d \n", zahlen[i], qzahlen[i]);
 	}
+	free(zahlen);
+	free(qzahlen);
 	return 0;
 }
