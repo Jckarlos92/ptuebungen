0bd098e0399f37b947118564f9e575857542d5b3
flxw <ich@flxw.de>
2013-01-28 16:37:48 +0000

    Mehrfache Typumwandlung vermeiden, Diskussion über Wertebereich hinzugefügt

--- Serie 6/aufgabe2.c
+++ Serie 6/aufgabe2.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main (int argc, char * argv[]) {
+    /* Die Zahlen 0-100 werden im Array zahlen gespeichert Dessen Typ ist char
+     * (also ein Byte), weil wir nicht mehr als 255 Werte benötigen */
 	unsigned char zahlen[100];
+    /* Die Quadratzahlen werden im qzahlen Array abgelegt Dessen Typ ist short,
+     * also zwei Bytes, da die Quadratzahlen bis zu 100*100=10000 groß werden */
 	short qzahlen[100];
 	int i;
 	for (i=0; i<100; i++) {
-		zahlen[i]=(char)(i+1);
+		zahlen[i]=(unsigned char)(i+1);
 		qzahlen[i]=(i+1)*(i+1);
 	} 
 	for (i=0; i<100; i++) {
