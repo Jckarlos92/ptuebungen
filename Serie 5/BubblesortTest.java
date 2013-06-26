4d6e9f8c908264bfdbebb391fb02016472ebfbcc
flxw <felix@flxw.de>
2013-06-26 14:42:12 +0200

    Altes Bubblesort.java weg, neue BubblesortText.java her
    Angepasst an neue print-Ausgabe und neue Sortierungsfunktion

--- Serie 5/BubblesortTest.java
+++ Serie 5/BubblesortTest.java
@@ -3,16 +3,16 @@ import java.util.Random;
 public class BubblesortTest {
 
 	public static void main (String[] args) throws Exception {
-		DoubleLinkedList<Integer> liste = new DoubleLinkedList<Integer>(10);
+		DoubleLinkedList<Integer> liste = new DoubleLinkedList<Integer>();
         Random rn = new Random();
 
-		for (int i=0, j = rn.nextInt(); i<j; i++) {
+		for (int i=0, j = rn.nextInt()%1000; i<10; i++) {
 			liste.addLast (new Integer(rn.nextInt()));
 		}
 
         System.out.println("UNSORTIERT:");
         liste.print();
-		liste.sort();
+		liste.bubblesort();
         System.out.println();
         System.out.println("SORTIERT:");
         liste.print();
